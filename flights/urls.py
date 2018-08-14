f5c3002fac6ee4db02ea4bc2413dd5ca71e3d0ef
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-08-14 23:07:53 +0200

    implement linking routes

--- flights/urls.py
+++ flights/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('airports/', views.airports),
-    path('flights/', views.flights),
-    path('flights/<int:flight_id>', views.flight),
-    path('passengers/', views.passengers),
-    path('passengers/<int:passenger_id>', views.passenger),
+    path('', views.index, name="index"),
+    path('airports/', views.airports, name="airports"),
+    path('flights/', views.flights, name="flights"),
+    path('flights/<int:flight_id>', views.flight, name="flight"),
+    path('passengers/', views.passengers, name="passengers"),
+    path('passengers/<int:passenger_id>', views.passenger, name="passenger"),
 ]
