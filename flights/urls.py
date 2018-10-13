186fb41ad67d7e9eabccf660dcb84ee7336b1baf
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-10-14 00:32:31 +0200

    static files, delete Media root

--- flights/urls.py
+++ flights/urls.py
@@ -21,6 +21,3 @@ urlpatterns = [
     path('passengers/<int:passenger_id>/book', views.book, name="book"),
     path('api/', include(router.urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
